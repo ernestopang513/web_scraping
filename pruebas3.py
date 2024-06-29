92cb14db628fda603a67f710dcf0dbc45847fcae
Ernesto_Pang <ernestopang513@hotmail.com>
2024-06-28 19:31:52 -0600

    Ubicacion de fechas

--- pruebas3.py
+++ pruebas3.py
@@ -20,11 +20,23 @@ driver.get(url)
 try:
     el_encino_mazatlan = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//div/a/h2[contains(text(), "El Encino")]')))
 
-   
-
-    print(el_encino_mazatlan.text)
+    #print(el_encino_mazatlan.text)
     
     el_encino_mazatlan.click()
+
+    condiciones_operativas = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//div[@class="menu-item"]/a[contains(text(), "Condiciones Operativas")]' ))).click()
+
+    #print(condiciones_operativas.text)
+
+    calidad_gas = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//div[@class="menu-item"]/a[contains(text(), "Calidad de Gas")]' )))
+
+    calidad_gas.click()
+
+    fecha_ultimo_registro = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//div[@class="loadedContent"]/div[@class="row"][3]/div[2]' )))
+
+    print(fecha_ultimo_registro.text)
+
+    time.sleep(5)
 
 finally:
     driver.quit()
