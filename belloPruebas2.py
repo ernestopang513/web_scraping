65dd012403afb95e7b9e931ba291a6fc11b5d3c1
Ernesto_Pang <ernestopang513@hotmail.com>
2024-07-06 22:42:32 -0600

    Fecha en color netro

--- belloPruebas2.py
+++ belloPruebas2.py
@@ -30,6 +30,7 @@ def crear_interfaz():
     root.geometry("400x300")
 
     # Paleta de colores CFE
+    negro_cfe = "#000000"
     verde_cfe = "#006341"
     blanco_cfe = "#FFFFFF"
     gris_cfe = "#A5A5A5"
@@ -49,7 +50,7 @@ def crear_interfaz():
 
     # Crear una etiqueta para el resultado
     #global etiqueta_resultado
-    etiqueta_resultado = ctk.CTkLabel(root, text="", font=("Arial", 14), text_color=gris_cfe)
+    etiqueta_resultado = ctk.CTkLabel(root, text="", font=("Arial", 14), text_color=negro_cfe)
     etiqueta_resultado.pack(pady=10, anchor="w")
 
     # Ejecutar el bucle principal de la ventana
